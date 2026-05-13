28555b9895e4f29f29585cccd91c417130292308
MohammadTuhin1 <muhammadtuhin.dev@gmail.com>
2026-05-13 16:12:27 +0600

    Design student list screen layout

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      theme: ThemeData(
+        brightness: Brightness.dark,
       ),
       home: HomeScreen(),
     );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,6 +10,31 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Student Data Management'),
+        centerTitle: true,
+        backgroundColor: Colors.grey.shade800,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+            itemCount: 3,
+            itemBuilder: (context, index){
+              return ListTile(
+                title: Text('Tuhin'),
+                subtitle: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Roll: 102'),
+                    Text('course: CSE'),
+                    Divider(),
+                  ],
+                ),
+              );
+            }),
+      ),
+    );
   }
 }
